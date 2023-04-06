02bfbfcff15189713662f01a610c1604382785f6
jamille <jamille.ara2003@gmail.com>
2023-04-06 16:10:14 -0300

    first commit

---
+++ javascript.js
@@ -0,0 +1,73 @@
+/* First chart */
+const ctx = document.getElementById('chart1');
+
+const labels = [
+
+    'Red',
+    'Blue',
+    'Yellow',
+    'Green',
+    'Purple',
+    'Orange'
+
+]
+
+const data = {
+    labels,
+    datasets: [{
+        data: [12, 19, 3, 5, 2, 3],
+        label: "Moedas",
+        fill: true,
+        backgroundColor:'cyan'
+    }]
+}
+
+const config = {
+    type: 'line',
+    data,
+    options: {
+        responsive: true
+    }
+};
+
+const chart1 = new Chart(ctx, config)
+
+/* Second chart */
+const ctx2 = document.getElementById('chart2');
+
+const labels2 = [
+
+    'Red',
+    'Blue',
+    'Yellow',
+    'Green',
+    'Purple',
+    'Orange'
+
+]
+
+const data2 = {
+    labels,
+    datasets: [{
+        data: [12, 19, 3, 5, 2, 3],
+        label: "Moedas",
+        fill: true,
+        backgroundColor:'cyan'
+    }]
+}
+
+const config2 = {
+    type: 'bar',
+    data,
+    options: {
+        responsive: true
+    }
+};
+
+const chart2 = new Chart(ctx2, config2)
+
+
+
+
+
+
